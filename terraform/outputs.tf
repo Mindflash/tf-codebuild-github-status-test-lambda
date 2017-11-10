648e9f09cca5ffa234d3c14fcd5ac78c706096e3
chris ludden <chris.ludden@gmail.com>
2017-11-10 08:12:31 -0700

    fix: adds additional terraform outputs (arn, invoke_arn, qualified_arn, version)

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,19 @@
+output "arn" {
+  value = "${aws_lambda_function.codebuild_github_status.arn}"
+}
+
 output "function_name" {
   value = "${aws_lambda_function.codebuild_github_status.function_name}"
+}
+
+output "invoke_arn" {
+  value = "${aws_lambda_function.codebuild_github_status.invoke_arn}"
+}
+
+output "qualified_arn" {
+  value = "${aws_lambda_function.codebuild_github_status.qualified_arn}"
+}
+
+output "version" {
+  value = "${aws_lambda_function.codebuild_github_status.version}"
 }
