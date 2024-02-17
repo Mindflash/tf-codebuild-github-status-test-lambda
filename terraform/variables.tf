52d56d0a7bf6f2f3f992907405793d21a332498c
Ricardo Enriquez <ricardo.enriquez@trakstar.com>
2024-02-16 22:54:07 -0600

    WIP

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,48 +1,48 @@
 variable "config_parameter_names" {
-  type        = "string"
+  type        = string
   description = "name of terraform managed ssm parameter"
 }
 
 variable "debug" {
-  type        = "string"
+  type        = string
   description = "node debug flag"
   default     = ""
 }
 
 variable "memory_size" {
-  type        = "string"
+  type        = string
   description = "lambda function memory limit"
   default     = 128
 }
 
 variable "name" {
-  type        = "string"
+  type        = string
   description = "stack name"
 }
 
 variable "node_env" {
-  type        = "string"
+  type        = string
   description = "node environment"
   default     = "production"
 }
 
 variable "region" {
-  type        = "string"
+  type        = string
   description = "aws region"
 }
 
 variable "s3_bucket" {
-  type        = "string"
+  type        = string
   description = "lambda artifact s3 bucket name"
 }
 
 variable "s3_key" {
-  type        = "string"
+  type        = string
   description = "lambda artifact s3 key"
 }
 
 variable "timeout" {
-  type        = "string"
+  type        = string
   description = "lambda function timeout"
   default     = 10
 }
